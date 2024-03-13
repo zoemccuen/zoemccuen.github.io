4fd8ffebb061e18045b9bbac3d6063a39ff228dc
Zoe McCuen <zoelenore@gmail.com>
2024-03-12 22:49:18 -0400

    12

--- csce242/assignments/12/script.js
+++ csce242/assignments/12/script.js
@@ -1,13 +1,12 @@
 class House {
     constructor(name, size, bedrooms, bathrooms, features, main_image, floor_plans) {
         this.name = name;
-        this.source = image;
         this.size = size;
         this.bedrooms = bedrooms;
         this.bathrooms = bathrooms;
-        this.features = features;
-        this.main_image = main_image;
-        this.floor_plans = floor_plans;
+        this.features = features; // Array
+        this.main_image = "https://portiaportia.github.io/json/images/house-plans/" + main_image;
+        this.floor_plans = floor_plans // Array
     }
 
     static async fetch(url) {
@@ -18,8 +17,8 @@ class House {
             }
             const houseData = await response.json();
             const house = houseData.map(houseData => {
-                const {size, bedrooms, bathrooms, features} = houseData;
-                return new House(size, bedrooms, bathrooms, features);
+                const { name, size, bedrooms, bathrooms, features, main_image, floor_plans } = houseData;
+                return new House(name, size, bedrooms, bathrooms, features, main_image, floor_plans);
             });
             return house;
         } catch (error) {
@@ -27,83 +26,68 @@ class House {
             return [];
         }
     }
-/* FIX THIS */
-    get expandedSection() {        
+
+    get renderHouse() {
+        // Make the main section to hold all the house info      
         const photoSection = document.createElement("section");
-        const target = "modal-" + this.name;        
-        photoSection.classList.add("w3-modal");
-        photoSection.id = target;
+        photoSection.classList.add("main-house-section");
 
-        const infoDiv = document.createElement("div");
-        infoDiv.classList.add("w3-modal-content");
-        infoDiv.classList.add("expanded-info");
+        // Make the div that holds the top part with the house info
+        const houseBox = document.createElement("div");
+        houseBox.classList.add("house-info"); // Flex container for top portion
 
-        const contentDiv = document.createElement("div");
- 
+        // Make the photo of the house - to the right and is 300px
         const photoImg = document.createElement("img");
-        photoImg.src = this.source;
-        photoImg.classList.add("photo-element-big");
+        photoImg.src = this.main_image;
+        photoImg.classList.add("photo-house");
 
-        const textBox = document.createElement("div");
-        textBox.classList.add("house-info");
+        // Name of the house as a heading
         const heading = document.createElement("p");
         heading.innerText = this.name;
         heading.classList.add("house-heading");
 
-        const houseFacts = document.createElement("p");
+        // Make div that holds the house photo and details (this is all the text and then the rest after)
+        const houseFacts = document.createElement("div");
+        const houseDetails = document.createElement("p");
         const houseFactText =
-                            "<p><b>Size:</b> " + this.size + "</p>" +
-                            "<p><b>Bedrooms:</b> " + this.bedrooms + "</p>" +
-                            "<p><b>Bathrooms:</b> " + this.bathrooms + "</p>" +
-                            "<p><b>Features:</b> " + this.features + "</p>";
+            "<p><b>Size:</b> " + this.size + "</p>" +
+            "<p><b>Bedrooms:</b> " + this.bedrooms + "</p>" +
+            "<p><b>Bathrooms:</b> " + this.bathrooms + "</p>" +
+            "<p><b>Features:</b> " + this.features + "</p>";
 
-        houseFacts.innerHTML = houseFactText;
-        textBox.appendChild(heading);
-        textBox.appendChild(houseFacts);
+        houseDetails.innerHTML = houseFactText;
+        houseBox.appendChild(photoImg);
+        houseFacts.appendChild(houseDetails);
+        houseBox.appendChild(houseFacts);
 
-        
-        const imageBox = document.createElement("div");
-        imageBox.classList.add("photo-big");    
-        imageBox.appendChild(photoImg); 
+        // Build the section which holds the floor plans. Should be flex to account for different #s of floors        
+        const floorPlans = document.createElement("div");
+        floorPlans.classList.add("floor-plans-container");
+        this.floor_plans.forEach((floorElement) => {
+            let floorDetail = document.createElement("div");
+            floorDetail.classList.add("floor-plans");
+            floorDetail.innerHTML = "<h3>" + floorElement.name + "</h3>";
+            let floorImage = document.createElement("img");
+            floorImage.classList.add("img-floor-plans");
+            floorImage.src = "https://portiaportia.github.io/json/images/house-plans/" + floorElement.image;
+            floorDetail.appendChild(floorImage);
+            floorPlans.appendChild(floorDetail);
+        });
 
-
+        // creates the section with heading, house, floorPlans, and all info as an "info card" section
         const infoCard = document.createElement("div");
-        infoCard.classList.add("info-card");
-        infoCard.appendChild(textBox);
-        infoCard.appendChild(imageBox);
+        infoCard.appendChild(heading);
+        infoCard.appendChild(houseBox);
+        infoCard.appendChild(floorPlans);
+        photoSection.appendChild(infoCard);
 
-        contentDiv.appendChild(infoCard);
-
-        infoDiv.appendChild(contentDiv);
-        photoSection.appendChild(infoDiv);
-
-        return photoSection;
-    }
-    
-
-    get section() {
-        const photoSection = document.createElement("section");
-        photoSection.classList.add("photo");
-        const target = "modal-" + this.name;
-        photoSection.onclick = () => { modalOpen(target); };
-        const photoImg = document.createElement("img");
-        photoImg.src = this.source;        
-        photoImg.classList.add("photo-element");
-        const titleLine = document.createElement("p");
-        titleLine.classList.add("photo-title");
-        titleLine.innerText = this.name;
-        photoSection.appendChild(titleLine);
-        photoSection.appendChild(photoImg);
         return photoSection;
     }
 }
 
-const modalOpen = (theName) => {
-    document.getElementById(theName).style.display = "block";
-}
-
+// adds everything together using the json url and returns the final products!
 const loadHouse = async () => {
-    const url = "https://portiaportia.github.io/json/house-plans.json" + new Date().getTime();
+    const url = "https://portiaportia.github.io/json/house-plans.json?" + new Date().getTime();
     try {
         const house = await House.fetch(url);
         return await house;
@@ -112,30 +96,19 @@ const loadHouse = async () => {
     }
 }
 
+// loads each aspect of the array to create the house and its other stuff
 const initGallery = async () => {
     let houseArray = await loadHouse();
-    let photoGallery = document.getElementById("image-gallery");
+    let photoGallery = document.getElementById("house-section");
 
     if (houseArray !== undefined && houseArray.length > 0) {
         houseArray.forEach((aHouse) => {
-            photoGallery.append(aHouse.section);
-            photoGallery.append(aHouse.expandedSection);
+            photoGallery.append(aHouse.renderHouse);
         })
     }
 }
 
-
-const showPlans = async () => {
-    let plans = await getPlans();
-    let plansSection = document.getElementById("floor_plans");
-    plans.type.forEach(getPlans(plans));
-};
-
-const getPlans = (plans) => {
-    let img = document.createElement("img");
-    section.append(img);
-};
-
+// loads everything up
 window.onload = () => {
     initGallery();
 };
