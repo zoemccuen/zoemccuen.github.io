30bf035141637ed36830e5ae5be620bf8b7d2e36
Zoe McCuen <zoelenore@gmail.com>
2024-03-19 19:31:56 -0400

    feedback

---
+++ csce242/projects/part6/form.js
@@ -0,0 +1,11 @@
+const submitEmail = (e) => {
+    e.preventDefault();
+    document.getElementById("results").classList.remove("hidden");
+  
+    const form = document.getElementById("form-email");
+    const emailName = form.elements["email-name"].value;
+  
+    console.log(emailName);
+  };
+  
+  document.getElementById("form-email").onsubmit = submitEmail;
